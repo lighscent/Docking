3cbbfa76091c3910134cf52ecfd0183e5476351a
lighscent <augustin.pro.prs@gmail.com>
2026-03-17 03:38:38 +0100

    fix: change default port to 1703

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
 const containersRouter = require("./routes/containers");
 const imagesRouter = require("./routes/images");
 const uiRouter = require("./routes/ui");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1703;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
